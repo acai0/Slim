11389d97765a3223a07381831676d9a3c154c995
acai0 <alicecai08@hotmail.com>
2022-03-11 11:44:27 +0100

    nettoyage

--- lbs.2021/lbs_commande_service/src/app/controller/TD1CommandController.php
+++ lbs.2021/lbs_commande_service/src/app/controller/TD1CommandController.php
@@ -13,32 +13,4 @@ class TD1CommandController extends Controller{
         "commandes"=>$this->commands];
         $rs = $rs->withHeader('Content-Type', 'application/json');
     }
-/*
-public function replaceCommand(){
-    $command_data = $rq->getParsed_Body();
-
-    if(!isset($command_data['nom_client'])
-    return Writter::json_error($rs,400, "missing data: nom client"));
-
-    if(!isset($command_data['mail_client'])
-    return Writter::json_error($rs,400, "missing data: nom client"));
-  // ... 
-
-  try{
-      $c= Command::Select(['id', 'nom', 'mail', 'livraison'])->findOrFail($args['id'];
-      $c->nom=filter_var($command_data['nom_client'], FILTER_SANITIZE_STRING))
-      //....
-      $c->livraison = \DateTime::createFromFormat('Y-m-d H:i', $command_data['livraison']['data'] . ' ' .
-      $command_data['livraison']['heure']);
-  }
-  $c->save();
-  return Writter::json_outpiut($rs, 204, 'commande');
-}
-catch (ModelNoteFoundException $e){
-    return Writter::json_error($rs, 404, 'commande')
-}
-catch(\Exception ê){
-    return Writter::json_error($rs, 500, $e->getMessage)
-}
-*/
 }
