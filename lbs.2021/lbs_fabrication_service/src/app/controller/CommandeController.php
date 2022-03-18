5101f9305cf13b4698d73e5a61eb5f4a5f59e371
acai0 <alicecai08@hotmail.com>
2022-03-18 11:02:32 +0100

    Pagination, Filtrage OK

--- lbs.2021/lbs_fabrication_service/src/app/controller/CommandeController.php
+++ lbs.2021/lbs_fabrication_service/src/app/controller/CommandeController.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use lbs\fab\app\models\Commande;
-use lbs\fab\app\output\Writer;
+use lbs\fab\app\errors\Writer;
 require __DIR__ .'/../models/Commande.php';
+require __DIR__ . '/../errors/Writer.php';
 class CommandeController
 {
     private $c;
@@ -20,7 +21,35 @@ class CommandeController
     // Récuperer toutes les commandes
     public function getAllCommande(Request $req, Response $resp): Response
     {
-        $commandes = Commande::select(['id', 'nom', 'montant', 'created_at', 'status'])->orderBy('livraison', 'DESC')->get();
+        $filtrage=$req->getQueryParams()['s']?? null;
+        $pagination= $req->getQueryParams()['page']?? 1;
+        $size=$req->getQueryParams()['size']?? 10;
+       // $commandes = Commande::select(['id', 'nom', 'montant', 'created_at', 'status'])->orderBy('livraison', 'DESC')->get();
+
+       if($filtrage){
+           $c= Commande::select(['id', 'nom', 'montant', 'created_at', 'status'])->where('status', '=',$filtrage)->orderBy('livraison', 'DESC')->get();
+           $count=count($c);
+        } else {
+            $c= Commande::select(['id', 'nom', 'montant', 'created_at', 'status'])->get();
+            $count=count($c);
+        }
+
+        $nb= $count/$size;
+        if($pagination){
+            if($pagination<0){
+                $c= $c->take($size);
+                $size=count($c);
+            }else if($pagination>$nb){
+                $skip=($page -1)*$size;
+                $c=$c->skip($skip)->take($size);
+                $size=count($c);
+            }
+            else{
+                $skip=($pagination-1)*$size;
+                $c=$c->skip($skip)->take($size);
+                $size=count($c);
+            }
+        }
 
         $commande_response = [];
         $commande = [];
@@ -38,7 +67,10 @@ class CommandeController
         
         $data_resp = [
             "type" => "collection",
-            "count" => count($commandes),
+            //"count" => count($commandes),
+            "count" => $count,
+            "size"=>$size,
+            "page"=>$pagination,
             "commandes" => $commande_response
         ];
 
@@ -93,4 +125,74 @@ class CommandeController
             return $clientError($req, $resp, 404, "Commande not found");
         }
     }
+/*
+    public function getCommandeStatus(Request $req, Response $resp): Response
+    {
+        $commandes = Commande::select(['id', 'nom', 'montant', 'created_at', 'status'])->orderBy('status')->get();
+
+        $commande_response = [];
+        $commande = [];
+        foreach ($commandes as $c) {
+            //le path d'une commande
+            $commandePath = $this->c->router->pathFor(
+                'getCommandeStatus',
+                ['s' => $c->status]
+            );
+
+            $commande["commande"] = $c;
+            $commande["links"] =  $commandePath;
+            array_push($commande_response, $commande);
+        }
+        
+        $data_resp = [
+            "type" => "collection",
+            "count" => count($commandes),
+            "commandes" => $commande_response
+        ];
+
+        $resp->getBody()->write(json_encode($data_resp));
+        return writer::json_output($resp, 200);
+    }
+
+    // TEST
+    public function check(Request $rq, Response $rs, callable $next){
+
+        //check si le status se trouve dans l'uri
+        $status = null;
+        $status = $rq->getQueryParam('s', null);
+
+        if(is_null($status)){
+            //check si le status se trouve dans un header applicatif
+            $api_header = $rq->getHeader('X-lbs-s');
+            $status = (isset($api_header[0])? $api_header[0] : null);
+        }
+
+
+        if(empty($status)){
+            //garder trace de l'erreur
+            ($this->c->get('logger.error')->error("Missing status in Commande route"));
+            return Writer::json_error($rs,403,"missing status");
+        }
+        //Get le status de la commande se trouvant dans la route
+        $commande_status = $rq->getAttribute('route')->getArgument('s');
+
+        $commande = null;
+        try{
+            $commande = Commande::where('status', '=', $commande_status)->firstOrFail();
+            if($commande->status !== $status){
+                ($this->c->get('logger.error'))->error("Invalid status in commande route($status)",[$commande->status]);
+                return Writer::json_error($rs, 403, "status invalid");
+            }
+        
+        }
+        catch(ModelNotFoundException $e){
+            ($this->c->get('logger.error'))->error("unknown commande");
+            return Writer::json_error($rs, 404, "Commande inconnue");
+        }
+
+        $rq = $rq->withAttribute('command',$commande);
+        return $next($rq,$rs);
+    }
+    */
+
 }
--- lbs.2021/lbs_fabrication_service/src/app/routes/routes_td.php
+++ lbs.2021/lbs_fabrication_service/src/app/routes/routes_td.php
@@ -3,5 +3,6 @@ use \lbs\fab\app\controller\CommandeController;
 use \lbs\fab\app\middleware\Middleware;
 require __DIR__ .'/../controller/CommandeController.php';
 require __DIR__ .'/../Middleware/Middleware.php';
-$app->get('/commands/{id}[/]',CommandeController::class. ':getCommande')->setName('getCommande')->add(middleware::class. ':putIntoJson');
-$app->get('/commands[/]',CommandeController::class. ':getAllCommande')->setName('getAllCommande')->add(middleware::class. ':putIntoJson');
+$app->get('/commandes/{id}[/]', CommandeController::class. ':getCommande')->setName('getCommande')->add(middleware::class. ':putIntoJson');
+$app->get('/commandes[/]', CommandeController::class. ':getAllCommande')->setName('getAllCommande')->add(middleware::class. ':putIntoJson');
+//$app->get('/commands[/]', CommandeController::class. ':getCommandeStatus')->setName('getCommandeStatus')->add(middleware::class. ':putIntoJson')->add(CommandeController::class. ':check');
